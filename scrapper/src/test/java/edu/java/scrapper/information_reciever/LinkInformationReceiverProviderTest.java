f870201e250c776e31c377dc5fbb7a8e40f35f9f
gavrilovds <dimagavrilov2004@yandex.ru>
2024-02-22 11:40:40 +0300

    scrapper: small refactoring

--- scrapper/src/test/java/edu/java/scrapper/information_reciever/LinkInformationReceiverProviderTest.java
+++ scrapper/src/test/java/edu/java/scrapper/information_reciever/LinkInformationReceiverProviderTest.java
@@ -4,12 +4,14 @@ import edu.java.client.github.GithubClientImpl;
 import edu.java.client.link_information.LinkInformationReceiver;
 import edu.java.client.link_information.LinkInformationReceiverProvider;
 import edu.java.link_type_resolver.LinkType;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LinkInformationReceiverProviderTest {
 
     @Test
+    @DisplayName("LinkInformationReceiverProvider#getReceiver basic test")
     public void getReceiver_shouldReturnCorrectReceiver() {
         LinkInformationReceiverProvider provider = new LinkInformationReceiverProvider();
         provider.registerReceiver(LinkType.GITHUB, new GithubClientImpl());
--- scrapper/src/test/java/edu/java/scrapper/information_reciever/LinkInformationReceiverTest.java
+++
@@ -1,5 +0,0 @@
-package edu.java.scrapper.information_reciever;
-
-public class LinkInformationReceiverTest {
-
-}
