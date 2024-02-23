d34c45194a168e88bf9406a0dddbae51c4f816ea
gavrilovds <dimagavrilov2004@yandex.ru>
2024-02-23 11:49:52 +0300

    scrapper: changes in ClientConfig

--- scrapper/src/main/java/edu/java/configuration/ClientConfig.java
+++ scrapper/src/main/java/edu/java/configuration/ClientConfig.java
@@ -3,19 +3,26 @@ package edu.java.configuration;
 import edu.java.client.github.GithubClient;
 import edu.java.client.link_information.LinkInformationReceiver;
 import edu.java.client.stackoverflow.StackOverflowClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-public class ClientConfig {
+@Configuration public class ClientConfig {
 
-    @Bean
-    public LinkInformationReceiver stackOverflowClient() {
-        return new StackOverflowClient();
+    @Value("${client.stackoverflow.base-url}") private String stackOverflowBaseUrl;
+    @Value("${client.github.base-url}") private String githubBaseUrl;
+
+    @Bean public LinkInformationReceiver stackOverflowClient() {
+        if (stackOverflowBaseUrl.isEmpty()) {
+            return new StackOverflowClient();
+        }
+        return new StackOverflowClient(stackOverflowBaseUrl);
     }
 
-    @Bean
-    public LinkInformationReceiver githubClient() {
-        return new GithubClient();
+    @Bean public LinkInformationReceiver githubClient() {
+        if (githubBaseUrl.isEmpty()) {
+            return new GithubClient();
+        }
+        return new GithubClient(githubBaseUrl);
     }
 }
