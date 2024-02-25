60ccfdf64eb685a66c122ed562fb659a3df6f8b1
gavrilovds <dimagavrilov2004@yandex.ru>
2024-02-25 22:01:28 +0300

    scrapper: fixes after review (2)

--- scrapper/src/main/java/edu/java/client/link/LinkInformationReceiverManager.java
+++ scrapper/src/main/java/edu/java/client/link/LinkInformationReceiverManager.java
@@ -1,23 +1,18 @@
 package edu.java.client.link;
 
 import edu.java.link_type_resolver.LinkType;
-import jakarta.annotation.PostConstruct;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class LinkInformationReceiverManager {
 
     private final Map<LinkType, LinkInformationProvider> receivers = new HashMap<>();
-    private final List<LinkInformationProvider> receiversList;
 
-    @PostConstruct
-    private void registerReceivers() {
+    public LinkInformationReceiverManager(List<LinkInformationProvider> receiversList) {
         receiversList.forEach(receiver -> receivers.put(receiver.getLinkType(), receiver));
     }
 
